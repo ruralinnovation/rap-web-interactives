38ad52937b555c9b3f2d534a7503dd6f092baf84
Camden Blatchly <camden.blatchly@ruralinnovation.us>
2023-08-18 14:48:53 -0600

    switch dist to docs

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve("dist"),
+    path: path.resolve("docs"),
     filename: "index_bundle.js"
   },
   module: {
